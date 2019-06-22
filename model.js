c369656ce54dc9f3f27526ed7bca95f27395c629
ChefyHase <ChefyHase@example.com>
2019-06-22 17:55:11 +0900

    update

--- model.js
+++ model.js
@@ -9,17 +9,17 @@ class Model {
   }
 
   build() {
-    const droprate = 0.1;
+    const droprate = 0.0;
     const input = tf.input({ shape: [2048] });
     const reshape = tf.layers.reshape({ targetShape: [64, 32, 1] }).apply(input);
 
-    const conv1 = tf.layers.conv2d({ filters: 16, kernelSize: 5, strides: 1, padding: 'same' }).apply(reshape);
+    const conv1 = tf.layers.conv2d({ filters: 128, kernelSize: 10, strides: 2, padding: 'same' }).apply(reshape);
     const activ1 = tf.layers.reLU().apply(conv1);
     const norm1 = tf.layers.batchNormalization().apply(activ1);
     const pooling1 = tf.layers.maxPooling2d({ poolSize: 2 }).apply(norm1);
     const dropout1 = tf.layers.dropout({ rate: droprate }).apply(pooling1);
 
-    const conv2 = tf.layers.conv2d({ filters: 8, kernelSize: 5, strides: 1, padding: 'same' }).apply(dropout1);
+    const conv2 = tf.layers.conv2d({ filters: 64, kernelSize: 5, strides: 2, padding: 'same' }).apply(dropout1);
     const activ2 = tf.layers.reLU().apply(conv2);
     const norm2 = tf.layers.batchNormalization().apply(activ2);
     const pooling2 = tf.layers.maxPooling2d({ poolSize: 2 }).apply(norm2);
@@ -27,10 +27,16 @@ class Model {
 
     const flatten = tf.layers.flatten().apply(dropout2);
 
-    const dense1 = tf.layers.dense({ units: 25 }).apply(flatten);
+    const dense1 = tf.layers.dense({ units: 64 }).apply(flatten);
     const activDense1 = tf.layers.reLU().apply(dense1);
 
-    const denseOutput = tf.layers.dense({ units: 4 }).apply(activDense1);
+    const dense2 = tf.layers.dense({ units: 25 }).apply(activDense1);
+    const activDense2 = tf.layers.reLU().apply(dense2);
+
+    const dense3 = tf.layers.dense({ units: 10 }).apply(activDense2);
+    const activDense3 = tf.layers.reLU().apply(dense3);
+
+    const denseOutput = tf.layers.dense({ units: 4 }).apply(activDense3);
     const activOutput = tf.layers.reLU().apply(denseOutput);
 
     const model = tf.model({ inputs: input, outputs: activOutput });
@@ -49,10 +55,10 @@ class Model {
       const { xs, ys } = this.data.nextBatch();
 
       const h = await this.model.fit(xs, ys, {
-        batchSize: 500,
-        epochs: 50,
+        batchSize: 100,
+        epochs: 100,
         shuffle: true,
-        validationSplit: 0.4
+        validationSplit: 0.3
       });
 
       console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);

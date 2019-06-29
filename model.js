d5978ac019c048dfa123daf3bb67cf10248f55ce
ChefyHase <ChefyHase@example.com>
2019-06-29 22:45:29 +0900

    update

--- model.js
+++ model.js
@@ -11,82 +11,50 @@ class Model {
 
   build() {
     const droprate = 0.0;
+    const lambda = 0.01;
+
     const input = tf.input({ shape: [1024] });
     const reshape = tf.layers.reshape({ targetShape: [1024, 1, 1] }).apply(input);
 
-    const conv1 = tf.layers.conv2d({
-      filters: 256,
-      kernelSize: [128, 1],
-      activation: 'relu',
+    const convLow = tf.layers.conv2d({
+      filters: 32,
+      kernelSize: [512, 1],
+      activation: 'linear',
       kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2',
-      strides: [4, 1]
+      padding: 'same',
+      kernelRegularizer: tf.regularizers.l2({ l2: lambda }),
+      strides: [1, 1]
     }).apply(reshape);
-    const norm1 = tf.layers.batchNormalization().apply(conv1);
-    const pool1 = tf.layers.maxPooling2d({ poolSize: [2, 1] }).apply(norm1);
-    const dropOut1 = tf.layers.dropout({ rate: droprate }).apply(pool1);
-
-    const conv2 = tf.layers.conv2d({
-      filters: 64,
-      kernelSize: [64, 1],
-      activation: 'relu',
+    const poolLow = tf.layers.globalMaxPooling2d({ name: 'poolLow' }).apply(convLow);
+    const convMid = tf.layers.conv2d({
+      filters: 32,
+      kernelSize: [24, 1],
+      activation: 'linear',
       kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2',
-      padding: 'same'
-    }).apply(dropOut1);
-    const norm2 = tf.layers.batchNormalization().apply(conv2);
-    const pool2 = tf.layers.maxPooling2d({ poolSize: [2, 1] }).apply(norm2);
-    const dropOut2 = tf.layers.dropout({ rate: droprate }).apply(pool2);
-
-    const conv3 = tf.layers.conv2d({
-      filters: 64,
-      kernelSize: [64, 1],
-      activation: 'relu',
+      padding: 'same',
+      kernelRegularizer: tf.regularizers.l2({ l2: lambda }),
+      strides: [1, 1]
+    }).apply(reshape);
+    const poolMid = tf.layers.globalMaxPooling2d({ name: 'poolMid' }).apply(convMid);
+    const convHigh = tf.layers.conv2d({
+      filters: 32,
+      kernelSize: [3, 1],
+      activation: 'linear',
       kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2',
-      padding: 'same'
-    }).apply(dropOut2);
-    const norm3 = tf.layers.batchNormalization().apply(conv3);
-    const pool3 = tf.layers.maxPooling2d({ poolSize: [2, 1] }).apply(norm3);
-    const dropOut3 = tf.layers.dropout({ rate: droprate }).apply(pool3);
+      padding: 'same',
+      kernelRegularizer: tf.regularizers.l2({ l2: lambda }),
+      strides: [1, 1]
+    }).apply(reshape);
+    const poolHigh = tf.layers.globalMaxPooling2d({ name: 'poolHigh' }).apply(convHigh);
 
-    const conv5 = tf.layers.conv2d({
-      filters: 128,
-      kernelSize: [64, 1],
-      activation: 'relu',
-      kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2',
-      padding: 'same'
-    }).apply(dropOut3);
-    const norm5 = tf.layers.batchNormalization().apply(conv5);
-    const pool5 = tf.layers.maxPooling2d({ poolSize: [2, 1] }).apply(norm5);
-    const dropOut5 = tf.layers.dropout({ rate: droprate }).apply(pool5);
+    const concat = tf.layers.concatenate().apply([poolLow, poolMid, poolHigh]);
 
-    const conv6 = tf.layers.conv2d({
-      filters: 512,
-      kernelSize: [64, 1],
-      activation: 'relu',
-      kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2',
-      padding: 'same'
-    }).apply(dropOut5);
-    const norm6 = tf.layers.batchNormalization().apply(conv6);
-    const pool6 = tf.layers.globalMaxPooling2d({ name: 'pool' }).apply(norm6);
-    const dropOut6 = tf.layers.dropout({ rate: droprate }).apply(pool6);
-
-    const dense1 = tf.layers.dense({
-      units: 64,
-      activation: 'relu',
-      kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2'
-    }).apply(dropOut6);
-    const dense1Drop = tf.layers.dropout({ rate: droprate }).apply(dense1);
     const denseOutput = tf.layers.dense({
       units: 3,
-      activation: 'relu',
+      activation: 'linear',
       kernelInitializer: 'heNormal',
-      kernelRegularizer: 'l1l2'
-    }).apply(dense1Drop);
+      kernelRegularizer: tf.regularizers.l2({ l2: lambda })
+    }).apply(concat);
 
     const model = tf.model({ inputs: input, outputs: denseOutput });
     model.summary();
@@ -98,40 +66,41 @@ class Model {
   lossParam(yTrue, yPred) {
     return tf.tidy(() => {
       console.log(yTrue.arraySync()[0], yPred.arraySync()[0]);
-      let lossParam = tf.metrics.meanSquaredError(yTrue, yPred);
+      let lossParam = this.rootMeanSquaredError(yTrue, yPred);
       return lossParam;
     });
   }
 
-  lossSound(x, raw, yPred) {
-    return tf.tidy(() => {
-      const sound = x.arraySync();
-      const params = yPred.arraySync();
-      const buffers = [];
-      for (let i = 0; i < sound.length; i++) {
-        const buffer = sound[i];
-        const param = {
-          freq: this.data.lin2log(params[i][0]),
-          gain: -1 * this.data.invNorm(params[i][1], -24.0, 24.0),
-          q: this.data.invNorm(params[i][2], 0.1, 24.0),
-          samplerate: this.data.samplerate
+  lossSound(x, raw, yTrue, yPred, lossParam) {
+    if (lossParam.mean().dataSync() < 0.05) {
+      return tf.tidy(() => {
+        const params = yPred.arraySync();
+        const paramsTrue = yTrue.arraySync();
+        const buffers = [];
+        for (let i = 0; i < raw.length; i++) {
+          const buffer = raw[i];
+          const param = {
+            freq: this.data.lin2log(params[i][0]),
+            gain: this.data.invNorm(paramsTrue[i][1], -24.0, 24.0),
+            q: this.data.invNorm(paramsTrue[i][2], 0.1, 24.0),
+            samplerate: this.data.samplerate
+          }
+          buffers.push(peaking.peaking(buffer, param));
         }
-        buffers.push(peaking.peaking(buffer, param));
-      }
-      const predFiltered = tf.tensor(buffers);
-      predFiltered.print()
-      raw = tf.tensor(raw);
-      const lossSound = tf.metrics.meanSquaredError(raw, predFiltered);
-      // raw.sub(predFiltered).abs().mean().print()
-
-      return lossSound;
-    });
+        const predFiltered = tf.tensor(buffers);
+        // Error to Signal Ratio
+        const lossSound = tf.div(tf.abs(raw.sub(predFiltered)).square().sum(), tf.abs(raw).square().sum());
+        // const lossSound = tf.metrics.meanSquaredError(x, predFiltered)//.div(tf.scalar(2));
+        return lossSound;
+      });
+    }
+    else return tf.zeros(lossParam.shape);
   }
 
   loss(x, raw, yTrue, yPred) {
     return tf.tidy(() => {
       const lossParam = this.lossParam(yTrue, yPred);
-      const lossSound = this.lossSound(x, raw, yPred);
+      const lossSound = this.lossSound(x, raw, yTrue, yPred, lossParam);
       const totalLoss = tf.mean(lossParam.add(lossSound));
 
       console.log('LossParam: ' + lossParam.mean().dataSync() + ' / LossSound: ' + lossSound.mean().dataSync());
@@ -140,17 +109,21 @@ class Model {
     });
   }
 
+  rootMeanSquaredError(yTrue, yPred) {
+    return tf.square(yTrue.sub(yPred)).mean().sqrt();
+  }
+
   async train() {
     this.build();
     // this.model = await tf.loadLayersModel('file://./eq-ai-1/model.json');
-    const optimizer = tf.train.adam(0.0002);
+    const optimizer = tf.train.adam(0.0001);
 
     let { xs, raw, ys } = this.data.nextBatch();
     let [xTrain, xVal] = this.data.valSplits(xs, 0.3);
     let [rawTrain, rawVal] = this.data.valSplits(raw, 0.3);
     let [ysTrain, ysVal] = this.data.valSplits(ys, 0.3);
 
-    const batchSize = 500;
+    const batchSize = 10;
 
     for (let j = 0; j < 5000; j++) {
       let trainLoss = await optimizer.minimize(() => {
